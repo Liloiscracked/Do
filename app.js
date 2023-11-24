91d1d1951be015f54bce397ff9fef56152b1175a
Liloiscracked <mr.moodyaljoudy@gmail.com>
2023-11-24 20:24:56 +0300

    added the redirection between sign in and signup

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ app.use(express.static('./login-signup'));
 
 // Define a route
 app.get('/', (req, res) => {
-    //res.sendFile("/Users/medoaljoudi/Desktop/Web Project/style.css");
     res.sendFile("/Users/medoaljoudi/Desktop/Web Project/login-signup/Signin.html");
-    //res.redirect("./Signin.html");
 });
-// app.get('/Signin',(req,res)=>{
-//     res.sendFile("/Users/medoaljoudi/Desktop/Web Project/Signin.html");
-// });
+ app.get('/Sign_UP.html',(req,res)=>{
+     res.sendFile("/Users/medoaljoudi/Desktop/Web Project/Signin.html");
+ });
 
 // Set the port to listen on
 const port = 3000;
