00bfa81d29c17381585229a899f6b2293a2dff13
mohammedaljaafari2019 <mohmmed1423@hotmail.com>
2023-12-11 03:32:28 +0300

    burger menu for singup page

--- login-signup/signup.js
+++ login-signup/signup.js
@@ -5,3 +5,28 @@ signUp.addEventListener('click',()=>{
     const username = document.getElementById("username").value
     console.log(email + username);
 })
+
+const menu = document.querySelector('#toggle');
+const menuItems = document.querySelector('#overlay');
+const menuContainer = document.querySelector('.menu-container');
+const menuIcon = document.querySelector('i');
+var animation = document.getElementById("ball");
+var password = document.getElementById("password");
+var shadow = document.getElementById("shadow");
+menu.addEventListener('click', () => {
+  console.log("hi");
+    menuItems.classList.toggle('open');
+    menuContainer.classList.toggle('full-menu');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-times');
+    if(password.style.display==="none"){
+        password.style.display="block"
+        animation.style.display="block"
+        shadow.style.display="block"
+    }
+    else{
+        password.style.display="none"
+        animation.style.display="none"
+        shadow.style.display="none"
+    }
+});
